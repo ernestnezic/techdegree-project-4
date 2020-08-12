2738bc054f060fe3412b24fc48d1463fb825159b
Ernest Nezic <ernest.nezic@gmail.com>
2020-08-12 18:12:03 +0200

    Part 1 added

--- oop_game-v2/js/Game.js
+++ oop_game-v2/js/Game.js
@@ -1,3 +1,50 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+ class Game {
+     
+    constructor() {
+         this.missed = 0;
+         this.phrases = this.createPhrases();
+         this.activePhrase = null;
+     }
+
+
+     /**
+      * Creates phrases for use in game
+      * @return {array} An array of phrases that could be used in the game
+      */
+     createPhrases() {
+        let phrasesArray = [];
+
+        phrasesArray.push(new Phrase('Down and out'));
+        phrasesArray.push(new Phrase('Let her rip'));
+        phrasesArray.push(new Phrase('Jumping the gun'));
+        phrasesArray.push(new Phrase('Lovey Dovey'));
+        phrasesArray.push(new Phrase('My cup of tea'));
+
+        return phrasesArray;
+     }
+
+
+     /**
+      * Selects random phrase from phrases property
+      * @return {Object} Phrase object chosen to be used
+      */
+     getRandomPhrase() {
+        return this.phrases[Math.floor(Math.random() * 5)];
+     }
+
+
+     /**
+      * Begins game by selecting a random phrase and displaying it to user
+      */
+     startGame() {
+        const overlayDiv = document.getElementById('overlay');
+        overlayDiv.style.display = 'none';
+        
+        this.activePhrase = this.getRandomPhrase();
+        this.activePhrase.addPhraseToDisplay();
+     }
+ }
--- oop_game-v2/js/Phrase.js
+++ oop_game-v2/js/Phrase.js
@@ -1,3 +1,32 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+ class Phrase {
+     
+    constructor(phrase) {
+        this.phrase = phrase.toLowerCase();
+     }
+
+    
+    /**
+     * Display phrase on game board
+     */
+    addPhraseToDisplay() {
+        const phraseDiv = document.getElementById('phrase');
+        const phraseUl = phraseDiv.firstElementChild;
+
+        for ( let i = 0; i < this.phrase.length; i++) {
+            const letter = document.createElement('li');
+            letter.innerHTML = this.phrase[i]
+
+            if (letter.innerHTML === ' ') {
+                letter.className = 'space'
+            } else {
+                letter.className = `hide letter ${this.phrase[i]}`
+            }
+
+            phraseUl.appendChild(letter);
+        }
+    }
+ }
