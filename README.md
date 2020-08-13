f614b4bb47ad80a9229acb20c4c5352d57ea403d
Ernest Nezic <ernest.nezic@gmail.com>
2020-08-13 22:43:55 +0200

    Added readme file comments

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # techdegree-project-4
  Treehouse Techdegree- Unit 4 Project: OOP Game
+
+Student
+    *Ernest Nežić
+
+Going for
+    *Meets Expectations
+
+Tested in
+    *Google Chrome for Mac
+    *Safari for Mac
--- oop_game-v2/js/app.js
+++ oop_game-v2/js/app.js
@@ -4,14 +4,14 @@
 
  let game;
 
-
+//Event listener that creates a new Game object and starts a game 
  document.getElementById('btn__reset').addEventListener('click', () => {
      game = new Game();
      game.startGame();
  });
 
+//Listening for in game keyboard clicks and handeling that interaction
  document.getElementById('qwerty').addEventListener('click', (event) => {
-
     if ( event.target.className === 'key' || event.target.className === 'chosen' || event.target.className === 'wrong') {
         game.handleInteraction(event.target);
         console.log(event.target);
